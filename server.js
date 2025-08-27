4cdbe3ed9560c04dfaafc5c4570e56bccdcf8065
Abhishek <abhisheks@pearlorganisation.com>
2025-08-27 16:16:23 +0530

    'config'

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const connectDB = require('./config/db');
+
+const path = require('path');
+const fs = require('fs');
+
+const cors = require('cors');
+dotenv.config();
+const app = express();
+
+
+
+// Middleware
+app.use(cors({
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.use(express.json({ limit: '200mb' }));
+app.use(express.urlencoded({ limit: '200mb', extended: true }));
+
+
+// Connect to MongoDB
+connectDB();
+
+
+
+
+// Default route
+app.get('/', (req, res) => res.send('API is running...'));
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
