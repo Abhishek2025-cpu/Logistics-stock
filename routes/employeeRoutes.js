e0f6fa71efffdf0a23a2116522d03f356641810b
Abhishek <abhisheks@pearlorganisation.com>
2025-08-28 18:03:08 +0530

    emp

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -5,9 +5,9 @@ const employeeController = require("../controllers/employeeController");
 
 // Admin/HR CRUD
 router.post("/register", upload.single("profilePic"), employeeController.registerEmployee);
-router.get("/", employeeController.getAllEmployees);
-router.put("/:employeeId", upload.single("profilePic"), employeeController.updateEmployee);
-router.delete("/:employeeId", employeeController.deleteEmployee);
+router.get("/all", employeeController.getAllEmployees);
+router.put("/update/:employeeId", upload.single("profilePic"), employeeController.updateEmployee);
+router.delete("/delete/:employeeId", employeeController.deleteEmployee);
 router.patch("/:employeeId/toggle-status", employeeController.toggleEmployeeStatus);
 
 // Employee Login
