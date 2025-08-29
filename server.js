e8c2af0ed0c893a33a80a5093098d7bee13be91d
Dev-pearl-2024 <dev@pearlorganisation.com>
2025-08-29 17:21:24 +0530

    create Department CRUD operation

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const vehiclesRoutes = require("./routes/vehiclesroutes");
 const Categoryrouter = require("./routes/categoris.js");
 const waste_Matrial_Router = require("./routes/WasteMatrial__Route.js");
 const attendanceRoutes = require("./routes/attendanceRoutes.js");
+const departmentRouter = require("./routes/departmaent__route.js");
 
 // Middleware
 app.use(
@@ -46,6 +47,7 @@ app.use("/api", attendanceRoutes);
 app.use("/api", clientsRoutes);
 app.use("/api", Categoryrouter);
 app.use("/api", waste_Matrial_Router);
+app.use("/api", departmentRouter);
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
